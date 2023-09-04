ced6db619064141090ecb8e51f0ba898f1a64660
wlqmfl <jiseung.hong@kaist.ac.kr>
2023-09-05 00:15:22 +0900

    import vader.js

--- today-s-mood/script.js
+++ today-s-mood/script.js
@@ -1,11 +1,24 @@
-const analyzer = new SentimentIntensityAnalyzer();
+// const analyzer = new SentimentIntensityAnalyzer();
 
-function analyzeSentiment() {
-    const inputText = prompt('Enter a sentence:');
-    const sentimentAnalysisResult = analyzer.polarity_scores(inputText);
+// function analyzeSentiment() {
+//     const inputText = prompt('Enter a sentence:');
+//     const sentimentAnalysisResult = analyzer.polarity_scores(inputText);
+//     sessionStorage.setItem('sentimentAnalysisResult', JSON.stringify(sentimentAnalysisResult));
+// }
+
+// document.getElementById('viewChartButton').addEventListener('click', function() {
+//     window.location.href = 'chart.html';
+// });
+const vader = require('./node_modules/vader-sentiment');
+
+document.getElementById('viewChartButton').addEventListener('click', function () {
+    // Get the user input from the input field
+    const inputText = document.getElementById('userText').value;
+
+    // Perform sentiment analysis on the input text
+    const sentimentAnalysisResult = vader.SentimentIntensityAnalyzer.polarity_scores(inputText);
+    console.log(intensity);
+
+    // Store the sentiment analysis result in session storage
     sessionStorage.setItem('sentimentAnalysisResult', JSON.stringify(sentimentAnalysisResult));
-}
-
-document.getElementById('viewChartButton').addEventListener('click', function() {
-    window.location.href = 'chart.html';
 });
