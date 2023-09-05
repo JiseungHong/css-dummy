e9bbfde4c7fedf19f9293f295cf7ba265261df9a
wlqmfl <jiseung.hong@kaist.ac.kr>
2023-09-05 13:15:30 +0900

    throw

--- today-s-mood/script.js
+++ today-s-mood/script.js
@@ -9,15 +9,21 @@
 // document.getElementById('viewChartButton').addEventListener('click', function() {
 //     window.location.href = 'chart.html';
 // });
-const vader = require('./node_modules/vader-sentiment');
+import SentimentIntensityAnalyzer from './node_modules/vader-sentiment/src/index.js';
+const analyzer = new SentimentIntensityAnalyzer()
+
+// var vader = require('./node_modules/vader-sentiment');
+// var analyzer = new vader();
 
 document.getElementById('viewChartButton').addEventListener('click', function () {
     // Get the user input from the input field
     const inputText = document.getElementById('userText').value;
 
+    console.log(inputText);
+
     // Perform sentiment analysis on the input text
-    const sentimentAnalysisResult = vader.SentimentIntensityAnalyzer.polarity_scores(inputText);
-    console.log(intensity);
+    const sentimentAnalysisResult = analyzer.polarity_scores(inputText);
+    console.log(sentimentAnalysisResult);
 
     // Store the sentiment analysis result in session storage
     sessionStorage.setItem('sentimentAnalysisResult', JSON.stringify(sentimentAnalysisResult));
